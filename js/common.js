390c63674c0b0c1366afd9495f234662d6c0b3a2
maikawaoka <mai.kawaoka@seattleconsulting.co.jp>
2021-04-04 21:46:59 +0900

    0404

--- js/common.js
+++ js/common.js
@@ -188,6 +188,9 @@ function resizeAddClass() {
     $('#team-detail').addClass('w-95').removeClass('w-60');
     $('.slider').css('width', '80%');
     $('#birthplace').addClass('row-cols-4').removeClass('row-cols-8');
+    $('.main-visual').attr('width', '170');
+    $('.main-visual').attr('height', '200');
+    document.getElementById("header-size").setAttribute('viewBox', '0 0 1000 400');
   } else {
     $('#pc-header').show();
     $('#sp-header').hide();
